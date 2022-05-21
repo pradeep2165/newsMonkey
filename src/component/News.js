18e6b1adaa58a2ea08736d161154d47a2ae2c77c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-05-21 21:35:35 +0530

    spinner implemented

--- src/component/News.js
+++ src/component/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export default class News extends Component {
   constructor(props) {
@@ -14,47 +15,61 @@ export default class News extends Component {
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c017e5b978b74f8b925d60c9d5a3c7c8&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.setState({ loading: true });
     let parseData = await data.json();
     this.setState({
       articles: parseData.articles,
       totalArticles: parseData.totalResults,
+      loading: false,
     });
   }
   handelNextClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c017e5b978b74f8b925d60c9d5a3c7c8&page=${
       this.state.page + 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({ articles: parseData.articles, page: this.state.page + 1 });
+    this.setState({
+      articles: parseData.articles,
+      page: this.state.page + 1,
+      loading: false,
+    });
   };
   handelPreviousClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c017e5b978b74f8b925d60c9d5a3c7c8&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({ articles: parseData.articles, page: this.state.page - 1 });
+    this.setState({
+      articles: parseData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });
   };
   render() {
     return (
       <div className="container align-items-center my-2">
         <div className="text-center">
           <h1>DailyNews - Top Headlines</h1>
+          {this.state.loading && <Spinner />}
         </div>
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title : ""}
-                  description={element.description ? element.description : ""}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
           ;
           <div className="d-flex justify-content-between">
             <button
