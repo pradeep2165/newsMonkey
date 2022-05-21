a81f8d87990b85b9ba04453ae8eebe3c06e41617
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-05-21 19:08:15 +0530

    data are fetched using example sampleitem and online new api

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -6,7 +6,15 @@ export default class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://static.cnbetacdn.com/article/2022/0521/83ce8818083bf3a.webp"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title} ...</h5>
             <p className="card-text">{description} ...</p>
