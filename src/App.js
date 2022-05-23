eac44d1a8c770c76d1744666a2f6396c7cba1e9c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-05-23 10:07:53 +0530

    api key hidden

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Navbar from "./component/Navbar";
 import News from "./component/News";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  const apikey = process.env.REACT_APP_NEWS_API;
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<News key="general" />} />
-        <Route exact path="/business" element={<News key="business" category="business" />} />
-        <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" />} />
-        <Route exact path="/general" element={<News key="general" category="" />} />
-        <Route exact path="/health" element={<News key="health" category="health" />} />
-        <Route exact path="/science" element={<News key="science" category="science" />} />
-        <Route exact path="/sports" element={<News key="sports" category="sports" />} />
-        <Route exact path="/technology" element={<News key="technology" category="technology" />} />
+        <Route exact path="/" element={<News apikey={apikey} key="general" />} />
+        <Route exact path="/business" element={<News apikey={apikey} key="business" category="business" />} />
+        <Route exact path="/entertainment" element={<News apikey={apikey} key="entertainment" category="entertainment" />} />
+        <Route exact path="/general" element={<News apikey={apikey} key="general" category="" />} />
+        <Route exact path="/health" element={<News apikey={apikey} key="health" category="health" />} />
+        <Route exact path="/science" element={<News apikey={apikey} key="science" category="science" />} />
+        <Route exact path="/sports" element={<News apikey={apikey} key="sports" category="sports" />} />
+        <Route exact path="/technology" element={<News apikey={apikey} key="technology" category="technology" />} />
       </Routes>
     </BrowserRouter>
   );
